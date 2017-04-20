008fc9054b549902ffe47d247e021077e45a18b5
mkfreeman <michael.k.freeman@gmail.com>
2017-04-19 21:34:38 -0700

    Pulled in changes from complete

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -1,52 +1,79 @@
 /* Create a treemap of country level measures. Inspiration drawn from https://bl.ocks.org/mbostock/4063582.
-*/
+ */
 $(function() {
-	// Read in your data. On success, run the rest of your code
-	d3.csv('data/prepped_data.csv', function(error, data){
-		// Setting defaults
-		var margin = {top: 40, right: 10, bottom: 10, left: 10},
-		    width = 960 - margin.left - margin.right,
-		    height = 500 - margin.top - margin.bottom;
+    // Read in your data. On success, run the rest of your code
+    d3.csv('data/prepped_data.csv', function(error, data) {
 
-		// variable to visualize
-		var measure = 'fertility_rate';
-		var color = d3.scale.category10();
+        // Setting defaults
+        var margin = {
+                top: 40,
+                right: 10,
+                bottom: 10,
+                left: 10
+            },
+            width = 960,
+            height = 500,
+            drawWidth = width - margin.left - margin.right,
+            drawHeight = height - margin.top - margin.bottom,
+            measure = 'fertility_rate'; // variable to visualize
 
-		// Wrapper div for the chart
-		var div = d3.select('#vis')
-								.append("div")
-								.attr('height', 600)
-								.attr('width', 600)
-								.style("left", margin.left + "px")
-								.style("top", margin.top + "px");
+        // Append a wrapper div for the chart
+        var div = d3.select('#vis')
+            .append("div")
+            .attr('height', height)
+            .attr('width', width)
+            .style("left", margin.left + "px")
+            .style("top", margin.top + "px");
 
-		// Function to arrange divs (will be called seperately for entering and updating)
-		var position = function() {
-			// Set the position of each div using the properties computed from the treemap function
-			this.style("left", function(d,i) {return d.x + "px"; })
-					.style("top", function(d) { return d.y + "px"; })
-					.style('width', function(d){return d.dx + 'px'})
-					.style("height", function(d) { return d.dy + "px"; })
-					.style("background", function(d) {return !d.values ? color(d.region) : null; })
-		}
+        /* ********************************** Create hierarchical data structure & treemap function  ********************************** */
 
-		// Construct a nest function using `d3.nest`, and create a variable with your nested data
-
-		// Construct a treemap function that sizes elements based on the current `measure`, and
-		// Make sure to specify how to retrieve the `children` from each element
+        // Nest your data *by region* using d3.nest()
 
 
-		// Write your `draw` function to bind data, and position elements
+        // Define a hierarchy for your data
 
 
-		// Call your draw function
+
+        // Create a *treemap function* that will compute your layout given your data structure
 
 
-		// Listen to change events on the input elements
-		$("input").on('change', function() {
+        /* ********************************** Create an ordinal color scale  ********************************** */
 
-			
-		});
+        // Get list of regions for colors
 
-	});
+
+        // Set an ordinal scale for colors
+
+
+        /* ********************************** Write a function to perform the data-join  ********************************** */
+
+        // Write your `draw` function to bind data, and position elements
+        var draw = function() {
+
+            // Redefine which value you want to visualize in your data by using the `.sum()` method
+
+
+            // (Re)build your treemap data structure by passing your `root` to your `treemap` function
+
+
+            // Bind your data to a selection of elements with class node
+            // The data that you want to join is array of elements returned by `root.leaves()`
+
+
+            // Enter and append elements, then position them using the appropriate *styles*
+
+        };
+
+        // Call your draw function
+        draw();
+
+        // Listen to change events on the input elements
+        $("input").on('change', function() {
+            // Set your measure variable to the value (which is used in the draw funciton)
+            measure = $(this).val();
+
+            // Draw your elements
+            draw();
+        });
+    });
 });
